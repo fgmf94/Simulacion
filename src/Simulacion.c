9268d6e056eb76ddb6253095bfc8f27dda5cd503
utnso <frank.beatle@gmail.com>
2016-09-24 12:57:34 -0300

    primer commit

---
+++ src/Simulacion.c
@@ -0,0 +1,26 @@
+/*
+ ============================================================================
+ Name        : Simulacion.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	int varV;
+	int varN;
+
+	printf("Ingrese el valor de la variable de control V:");
+	scanf("%d", &varV);
+
+	printf("Ingrese el valor de la variable de control N:");
+	scanf("%d", &varN);
+
+	return EXIT_SUCCESS;
+}
